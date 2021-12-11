8c81761770876c0fbc8bcd383e21e0d1051d5de4
john_s <john.s@clarusway.com>
2021-12-11 17:37:07 +0300

    useParams added

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={ <Navigate to='/welcome' /> }/>
-          <Route path='/welcome' element={<Welcome/>}/>
+          <Route path='/welcome/*' element={<Welcome/>}>
+            <Route path='new-user' element={<p>Welcome new user!</p>} />
+          </Route>
           <Route path='/products' element={<Products/>}/>
-          <Route path='/productDetail' element={<ProductDetail/>}/>
+          <Route path='/products/:productId' element={<ProductDetail/>}/>
         </Routes>
       </main>
     </div>
